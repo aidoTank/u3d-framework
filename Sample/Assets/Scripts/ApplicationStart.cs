f75f46ce6dc0f7cfec48faf9ffe95f214a5cc895
abaojin <abaojin@gmail.com>
2017-03-27 14:55:30 +0800

    frame dir modify
    Signed-off-by: abaojin <abaojin@gmail.com>

---
+++ Sample/Assets/Scripts/ApplicationStart.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine;
+using UnityEngine;
+
+namespace GameLogic
+{
+    public class ApplicationStart : MonoBehaviour
+    {
+        void Start()
+        {
+            GameLog.Info("---------------log------------");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
