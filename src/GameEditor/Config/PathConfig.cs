244c9b1391a1c30f5ff93e9b6b425a44eebfe056
abaojin <abaojin@gmail.com>
2020-06-06 11:52:53 +0800

    new frame frame work
    Signed-off-by: abaojin <abaojin@gmail.com>

--- src/GameEditor/Config/PathConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameEditor
-{
-    public class PathConfig
-    {
-        public static string RES_OUTPUT = "Bin/AssetBundle";
-
-        public static string APP_OUTPUT = "Bin/Application";
-    }
-}
--- src/GameEngine/Conf/Pattern/Iconf.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameEngine
-{
-    public interface Iconf<T> where T: IReader
-    {
-        void OnLoad(T reader);
-    }
-}
