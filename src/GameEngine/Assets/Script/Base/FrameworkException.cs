dd02b1ef40fc42ce15afeb74c612a493dfbedb28
abaojin <abaojin@gmail.com>
2020-06-06 14:32:51 +0800

    base module
    Signed-off-by: abaojin <abaojin@gmail.com>

---
+++ src/GameEngine/Assets/Script/Base/FrameworkException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Framework
+{
+    /// <summary>
+    /// 框架异常类
+    /// </summary>
+    [Serializable]
+    public class FrameworkException : Exception
+    {
+        public FrameworkException() : base()
+        {
+
+        }
+
+        public FrameworkException(string message) : base(message)
+        {
+
+        }
+
+        public FrameworkException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public FrameworkException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
+
---
+++ src/GameEngine/Assets/Script/Base/FrameworkModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    internal abstract class FrameworkModule
+    {
+        internal virtual int Priority
+        {
+            get {
+                return 0;
+            }
+        }
+
+        internal abstract void Update(float elapseSeconds, float realElapseSeconds);
+
+        internal abstract void Shutdown();
+    }
+}
